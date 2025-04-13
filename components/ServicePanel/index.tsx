82502f023b6382aae4e182ee5037e7d75de778fb
William Parker <70612211+Jaspann@users.noreply.github.com>
2025-04-12 17:02:53 -0700

    Revert service panel design

--- components/ServicePanel/index.tsx
+++ components/ServicePanel/index.tsx
@@ -2,8 +2,30 @@ import { Stack, Divider } from '@mui/material';
 
 import Youtube from '../service/Youtube';
 import Twitch from '../service/Twitch';
+import React, { JSX } from 'react';
 
 function ServicePanel() {
+  const [currentUrl, setCurrentUrl] = useState<URL | null>(null);
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const urlStr = tabs[0]?.url ?? '';
+      if (urlStr) {
+        setCurrentUrl(new URL(urlStr));
+      }
+    });
+  }, []);
+
+  const services: Record<string, JSX.Element> = {
+    [youtubeUrl]: <Youtube />,
+    [twitchUrl]: <Twitch />,
+  };
+
+  const hostService = currentUrl ? services[currentUrl.hostname] : null;
+  const alternativeServices = Object.entries(services)
+    .filter(([url]) => url !== currentUrl?.hostname)
+    .map(([url, component]) => React.cloneElement(component, { key: url }));
+
   return (
     <Stack
       direction="row"
@@ -12,55 +34,23 @@ function ServicePanel() {
         alignItems: 'center',
       }}
     >
-      <Youtube />
-      <Divider orientation="vertical" variant="middle" flexItem />
-      <Twitch />
+      {hostService}
+      {hostService && (
+        <Divider orientation="vertical" variant="middle" flexItem />
+      )}
+      <Stack
+        divider={<Divider flexItem />}
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {alternativeServices.map((element, index) => (
+          <React.Fragment key={index}>{element}</React.Fragment>
+        ))}
+      </Stack>
     </Stack>
   );
 }
 
 export default ServicePanel;
-
-// Solution for more then two services:
-//
-// const [currentUrl, setCurrentUrl] = useState<URL | null>(null);
-//
-// useEffect(() => {
-//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     const urlStr = tabs[0]?.url ?? '';
-//     if (urlStr) {
-//       setCurrentUrl(new URL(urlStr));
-//     }
-//   });
-// }, []);
-//
-// const services: Record<string, JSX.Element> = {
-//   [youtubeUrl]: <Youtube />,
-//   [twitchUrl]: <Twitch />
-//   };
-
-//   const hostService = currentUrl ? services[currentUrl.hostname] : null;
-//   const alternativeServices = Object.entries(services)
-//   .filter(([url]) => url !== currentUrl?.hostname)
-//   .map(([url, component]) => React.cloneElement(component, { key: url }));
-//
-// return (
-//   <Stack direction="row"
-//   sx={{
-//     justifyContent: "center",
-//     alignItems: "center",
-//   }}>
-//     {hostService}
-//     {hostService && <Divider orientation="vertical" variant="middle" flexItem />}
-//     <Stack divider={<Divider flexItem />}
-//       sx={{
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       {alternativeServices.map((element, index) => (
-//         <React.Fragment key={index}>{element}</React.Fragment>
-//       ))}
-//     </Stack>
-//   </Stack>
-// );
